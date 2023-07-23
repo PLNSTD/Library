52d9a94c903a00aee6cfe8ea92bff0e74f3182a8
Plidher <65611683+PLNSTD@users.noreply.github.com>
2023-07-22 23:49:53 -0400

    added modal to disable background when popup window is shown

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const mainContainer = document.querySelector('#main-container');
 const addBookBtn = document.querySelector('#add-book-button');
+const modal = document.querySelector('.modal');
+
+$('.overlay').on('click', function(e) {
+    if (e.target !== this) {
+        return;
+    }
+    $('overlay').hide();
+})
 
 addBookBtn.addEventListener('click', function(e) {
     const newBookContainer = document.createElement('div');
-    newBookContainer.classList.add('bookCard');
+    
     const addNewBookP = document.createElement('p');
     const titleInputField = document.createElement('input');
     const authorInputField = document.createElement('input');
@@ -16,12 +24,17 @@ addBookBtn.addEventListener('click', function(e) {
     pagesInputField.setAttribute('placeholder', 'Pages');
     submitBtn.textContent = 'Submit';
     submitBtn.classList.add('submit-button');
-    
-    newBookContainer.appendChild(addNewBookP);
-    newBookContainer.appendChild(titleInputField);
-    newBookContainer.appendChild(authorInputField);
-    newBookContainer.appendChild(pagesInputField);
-    newBookContainer.appendChild(submitBtn);
+    submitBtn.addEventListener('click', function(e) {
+        modal.textContent = '';
+        $('.overlay').hide();
+    })
 
-    mainContainer.appendChild(newBookContainer);
+    modal.appendChild(addNewBookP);
+    modal.appendChild(titleInputField);
+    modal.appendChild(authorInputField);
+    modal.appendChild(pagesInputField);
+    modal.appendChild(submitBtn);
+
+    //modal.appendChild(newBookContainer);
 });
+
